976912cc2d5d605148e5cc6cb82ff64d7a4036a0
Yana Nechpal <y.nechpal@ya.ru>
2023-02-21 22:18:10 +0300

    hotfix первое ревью: позиционирование, поп-ап, картинки, удалены некоторые высоты элементов и лишние файлы

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,41 @@
-const editProfileButton = document.querySelector('.profile__edit-button');
-const popupEdit = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup'); // поп-ап
 
+const profileButtonEdit = document.querySelector('.profile__edit-button'); // кнопка редактирования
+const profileCloseButtonEdit = document.querySelector('.popup__close-button'); // кнопка закрытия поп-апа
+
+const profileName = document.querySelector('.profile__name'); // данные профиля в строке "имя"
+const profileAboutYourself = document.querySelector('.profile__about-yourself'); // данные профиля в строке "о себе"
+
+const formElement = document.querySelector('.popup__input-form'); // форма для ввода данных
+
+const userNameInput = document.querySelector('.popup__input_type_name'); // поле ввода "имя"
+const userAboutYourselfInput = document.querySelector('.popup__input_type_about-yourself'); // поле ввода "о себе"
+
+//функции: открытия и закрытия поп-апа (добавление и удаление класса), обработчик «отправки» формы
 function openPopup(popup) {
     popup.classList.add('popup_opened');
+    userNameInput.value = profileName.textContent; // имя профиля присваиваем (=) значению (.value) поля ввода имени в форме поп-апа
+    userAboutYourselfInput.value = profileAboutYourself.textContent; // аналогично с "о себе"
 }
 
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
 }
-editProfileButton.addEventListener('click', function() {
-    openPopup(popupEdit);
-});
-const editProfileCloseButton = document.querySelector('.popup__close-button');
-editProfileCloseButton.addEventListener('click', function() {
-    closePopup(popupEdit);
-});
-const userName = 'Жак-Ив Кусто';
-const userAboutYourself = 'Исследователь океана';
-const profileName = document.querySelector('.profile__name');
-const profileAboutYourself = document.querySelector('.profile__about-yourself');
-const userNameInput = document.querySelector('.popup__input_name');
-userNameInput.value = userName;
-const userAboutYourselfInput = document.querySelector('.popup__input_about_yourself');
-userAboutYourselfInput.value = userAboutYourself;
-const editProfileElements = document.querySelector('.popup__container');
-const inputName = document.querySelector('.popup__input_name');
-const inputAboutYourself = document.querySelector('.popup__input_about_yourself');
 
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-    profileName.textContent = inputName.value;
-    profileAboutYourself.textContent = inputAboutYourself.value;
+    evt.preventDefault(); // отмена отправки формы
+    profileName.textContent = userNameInput.value; // новое содержимое-значение (value) присваивается полю ввода имени - свойству textContent переменной 
+    profileAboutYourself.textContent = userAboutYourselfInput.value; // аналогично
     closePopup(popupEdit);
 }
-editProfileElements.addEventListener('submit', handleFormSubmit);
+
+// обработчики события: по клику открытие и закрытие, отправка данных (submit)
+profileButtonEdit.addEventListener('click', function() {
+    openPopup(popupEdit);
+});
+
+profileCloseButtonEdit.addEventListener('click', function() {
+    closePopup(popupEdit);
+});
+
+formElement.addEventListener('submit', handleFormSubmit);
