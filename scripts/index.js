995d7355ec2c592a6413c77c39eed1440a6f7d9b
Yana Nechpal <y.nechpal@ya.ru>
2023-03-02 15:08:19 +0300

    feature/ удаление карточек, отметка "нравится",  карточки формируются js

--- scripts/index.js
+++ scripts/index.js
@@ -42,63 +42,81 @@ profileCloseButtonEdit.addEventListener("click", function () {
 
 formElement.addEventListener("submit", handleFormSubmit);
 
-
-// СПРИНТ 5 
+// СПРИНТ 5
 // Карточки
 // Создание массива с карточками
 const photoCards = [
     {
-      title: 'Морской котик',
-      alt: 'Морской котик погружается в воду',
-      image: 'https://images.unsplash.com/photo-1504436965013-d4d16fd26afa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        title: "Морской котик",
+        alt: "Морской котик погружается в воду",
+        image: "https://images.unsplash.com/photo-1504436965013-d4d16fd26afa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
-      title: 'Синий кит',
-      alt:'Синий кит',
-      image: 'https://images.unsplash.com/photo-1566392421529-bdba2f3d933d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+        title: "Синий кит",
+        alt: "Синий кит",
+        image: "https://images.unsplash.com/photo-1566392421529-bdba2f3d933d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
     },
     {
-      title: 'Медуза',
-      alt: 'Медуза',
-      image: 'https://images.unsplash.com/photo-1626288937173-9506afb2fc7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+        title: "Медуза",
+        alt: "Медуза",
+        image: "https://images.unsplash.com/photo-1626288937173-9506afb2fc7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
     },
     {
-      title: 'Коралловый риф',
-      alt: 'Коралловый риф',
-      image: 'https://images.unsplash.com/photo-1582967788606-a171c1080cb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+        title: "Коралловый риф",
+        alt: "Коралловый риф",
+        image: "https://images.unsplash.com/photo-1582967788606-a171c1080cb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
     },
     {
-      title: 'Скат',
-      alt: 'Скат',
-      image: 'https://images.unsplash.com/photo-1582012446386-1682363c2f1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+        title: "Скат",
+        alt: "Скат",
+        image: "https://images.unsplash.com/photo-1582012446386-1682363c2f1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
     },
     {
-      title: 'Морская черепаха',
-      alt: 'Морская черепаха',
-      image: 'https://images.unsplash.com/photo-1591025207163-942350e47db2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    }
-  ];
+        title: "Морская черепаха",
+        alt: "Морская черепаха",
+        image: "https://images.unsplash.com/photo-1591025207163-942350e47db2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+];
 
-  const cardsContainer = document.querySelector('.photos');
+// рефакторинг карточек с помощью tamplate
+const cardsContainer = document.querySelector(".photos");
+// создание карточки
+const createCard = (card) => {
+    const newCard = document
+        .querySelector("#cardTemplate")
+        .content.cloneNode(true);
+    const cardTitle = newCard.querySelector(".photo__title");
+    const cardImage = newCard.querySelector(".photo__image");
+    const cardDeleteButton = newCard.querySelector(".photo__delete-button");
+    const cardLikeButton = newCard.querySelector(".photo__like-button");
 
-  const createCard = (card) => {
-    const newCard = document.querySelector('#cardTemplate').content.cloneNode(true);
-    const cardTitle = newCard.querySelector('.photo__title');
     cardTitle.textContent = card.title;
-    const cardImage = newCard.querySelector('.photo__image');
-    cardImage.setAttribute('src', card.image);
-    cardImage.setAttribute('alt', card.alt);
-    const deleteButton = newCard.querySelector('.photo__delete-button');
-    deleteButton.addEventListener('click', handleDeleteButtonClick);
+    cardImage.setAttribute("src", card.image);
+    cardImage.setAttribute("alt", card.alt);
+
+    // обработчик события: удаление карточки
+    cardDeleteButton.addEventListener("click", handleDeleteButtonClick);
+    
+    // обработчик события: нравится карточка
+    cardLikeButton.addEventListener("click", handleLikeButtonClick);
+    
     cardsContainer.append(newCard);
-  }
+};
 
-  photoCards.forEach(createCard)
+//цикл для массива с определенной функцией (метод forEach принимает функцию в качестве аргумента)
+photoCards.forEach(createCard);
 
-  function handleDeleteButtonClick(evt) {
+// фунуция удаления карточки
+ function handleDeleteButtonClick(evt) {
     const button = evt.target;
-    const photo = button.closest('.photo');
+    const photo = button.closest(".photo");
     photo.remove();
-  }
+}
 
+// функция лайка карточки
+function handleLikeButtonClick(evt) {
+    const buttonLike = evt.target;
+    buttonLike.classList.toggle("photo__like-button_active");
+}
 
+//Добавление карточки
