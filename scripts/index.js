ac767b2936e4027244052f7199b22b952a94e8d1
Yana Nechpal <y.nechpal@ya.ru>
2023-03-07 01:07:11 +0300

    feat and refactor: улучшена функциональность проекта - 1 итерация с5

--- scripts/index.js
+++ scripts/index.js
@@ -1,122 +1,189 @@
-const popupEdit = document.querySelector(".popup"); // поп-ап
-
-const profileButtonEdit = document.querySelector(".profile__edit-button"); // кнопка редактирования
-const profileCloseButtonEdit = document.querySelector(".popup__close-button"); // кнопка закрытия поп-апа
-
-const profileName = document.querySelector(".profile__name"); // данные профиля в строке "имя"
-const profileAboutYourself = document.querySelector(".profile__about-yourself"); // данные профиля в строке "о себе"
-
-const formElement = document.querySelector(".popup__input-form"); // форма для ввода данных
-
-const userNameInput = document.querySelector(".popup__input_type_name"); // поле ввода "имя"
+//Поп-апа профиля
+const profilePopup = document.querySelector('.popup_type_edit-profile')
+//Кнопка открытия и закрытия поп-апа редактирования профиля
+const profileButtonEdit = document.querySelector('.profile__edit-button')
+const popupCloseButtonProfile = document.querySelector(
+  '.popup__close-button_type_profile'
+)
+//Поля формы редактирования профиля
+const formElementProfile = document.querySelector(
+  '.popup__input-form_type_edit-profile'
+) // форма для ввода данных
+const profileName = document.querySelector('.profile__name') // данные профиля в строке "имя"
+const profileAboutYourself = document.querySelector('.profile__about-yourself') // данные профиля в строке "о себе"
+const userNameInput = document.querySelector('.popup__input_type_name') // поле ввода "имя"
 const userAboutYourselfInput = document.querySelector(
-    ".popup__input_type_about-yourself"
-); // поле ввода "о себе"
+  '.popup__input_type_about-yourself'
+) // поле ввода "о себе"
 
-//функции: открытия и закрытия поп-апа (добавление и удаление класса), обработчик «отправки» формы
-function openPopup(popup) {
-    popup.classList.add("popup_opened");
-    userNameInput.value = profileName.textContent; // имя профиля присваиваем (=) значению (.value) поля ввода имени в форме поп-апа
-    userAboutYourselfInput.value = profileAboutYourself.textContent; // аналогично с "о себе"
+//Общие функции: открытия и закрытия поп-апа (добавление и удаление класса)
+function openPopup (popup) {
+  popup.classList.add('popup_opened')
 }
 
-function closePopup(popup) {
-    popup.classList.remove("popup_opened");
+function closePopup (popup) {
+  popup.classList.remove('popup_opened')
 }
 
-function handleFormSubmit(evt) {
-    evt.preventDefault(); // отмена отправки формы
-    profileName.textContent = userNameInput.value; // новое содержимое-значение (value) присваивается полю ввода имени - свойству textContent переменной
-    profileAboutYourself.textContent = userAboutYourselfInput.value; // аналогично
-    closePopup(popupEdit);
+// Функция редактирования профиля: перезапись данных, присвоение
+function handleProfileEdit (evt) {
+  evt.preventDefault() // отмена отправки формы
+  profileName.textContent = userNameInput.value // новое содержимое-значение (value) присваивается полю ввода имени - свойству textContent переменной
+  profileAboutYourself.textContent = userAboutYourselfInput.value // аналогично
+  closePopup(profilePopup)
 }
 
-// обработчики события: по клику открытие и закрытие, отправка данных (submit)
-profileButtonEdit.addEventListener("click", function () {
-    openPopup(popupEdit);
-});
+// Обработчики события для профиля по клику: открытие и закрытие
+profileButtonEdit.addEventListener('click', function () {
+  openPopup(profilePopup)
+  userNameInput.value = profileName.textContent // имя профиля присваиваем (=) значению (.value) поля ввода имени в форме поп-апа
+  userAboutYourselfInput.value = profileAboutYourself.textContent // аналогично с "о себе"
+})
 
-profileCloseButtonEdit.addEventListener("click", function () {
-    closePopup(popupEdit);
-});
+popupCloseButtonProfile.addEventListener('click', function () {
+  closePopup(profilePopup)
+})
 
-formElement.addEventListener("submit", handleFormSubmit);
+// Обработчик «отправки» данных формы (submit)
+formElementProfile.addEventListener('submit', handleProfileEdit)
 
 // СПРИНТ 5
 // Карточки
 // Создание массива с карточками
 const photoCards = [
-    {
-        title: "Морской котик",
-        alt: "Морской котик погружается в воду",
-        image: "https://images.unsplash.com/photo-1504436965013-d4d16fd26afa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    },
-    {
-        title: "Синий кит",
-        alt: "Синий кит",
-        image: "https://images.unsplash.com/photo-1566392421529-bdba2f3d933d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
-    },
-    {
-        title: "Медуза",
-        alt: "Медуза",
-        image: "https://images.unsplash.com/photo-1626288937173-9506afb2fc7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-    },
-    {
-        title: "Коралловый риф",
-        alt: "Коралловый риф",
-        image: "https://images.unsplash.com/photo-1582967788606-a171c1080cb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-    },
-    {
-        title: "Скат",
-        alt: "Скат",
-        image: "https://images.unsplash.com/photo-1582012446386-1682363c2f1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-    },
-    {
-        title: "Морская черепаха",
-        alt: "Морская черепаха",
-        image: "https://images.unsplash.com/photo-1591025207163-942350e47db2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    },
-];
+  {
+    title: 'Морская черепаха',
+    alt: 'Морская черепаха',
+    image:
+      'https://images.unsplash.com/photo-1591025207163-942350e47db2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    title: 'Скат',
+    alt: 'Скат',
+    image:
+      'https://images.unsplash.com/photo-1582012446386-1682363c2f1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+  },
+  {
+    title: 'Коралловый риф',
+    alt: 'Коралловый риф',
+    image:
+      'https://images.unsplash.com/photo-1582967788606-a171c1080cb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  },
+  {
+    title: 'Медуза',
+    alt: 'Медуза',
+    image:
+      'https://images.unsplash.com/photo-1626288937173-9506afb2fc7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  },
+  {
+    title: 'Синий кит',
+    alt: 'Синий кит',
+    image:
+      'https://images.unsplash.com/photo-1566392421529-bdba2f3d933d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+  },
+  {
+    title: 'Морской котик',
+    alt: 'Морской котик погружается в воду',
+    image:
+      'https://images.unsplash.com/photo-1504436965013-d4d16fd26afa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  }
+]
 
 // рефакторинг карточек с помощью tamplate
-const cardsContainer = document.querySelector(".photos");
+const cardsContainer = document.querySelector('.photos')
+//Добавление поп-апа карточки
+const formElementCard = document.querySelector(
+  '.popup__input-form_type_add-card'
+)
+const cardPopup = document.querySelector('.popup_type_add-card')
+const placeNameInput = document.querySelector('.popup__input_type_name-place')
+const placeLinkInput = document.querySelector('.popup__input_type_link-place')
+//Кнопка открытия и закрытия поп-апа добавление карточки
+const popupAddCardButton = document.querySelector('.profile__add-button')
+const popupCloseButtonCard = document.querySelector(
+  '.popup__close-button_type_card'
+)
+
+// Попап изображения
+const imagePopupOpen = document.querySelector('.popup_type_image-open')
+const imagePopup = document.querySelector('.popup__image')
+const imageTitle = document.querySelector('.popup__image-title')
+const popupCloseButtonImageOpen = document.querySelector(
+  '.popup__close-button_type_image-open'
+)
+
 // создание карточки
-const createCard = (card) => {
-    const newCard = document
-        .querySelector("#cardTemplate")
-        .content.cloneNode(true);
-    const cardTitle = newCard.querySelector(".photo__title");
-    const cardImage = newCard.querySelector(".photo__image");
-    const cardDeleteButton = newCard.querySelector(".photo__delete-button");
-    const cardLikeButton = newCard.querySelector(".photo__like-button");
+const createCard = card => {
+  const newCard = document
+    .querySelector('#cardTemplate')
+    .content.cloneNode(true) // содержимое template передается методом .content
+  const cardTitle = newCard.querySelector('.photo__title')
+  const cardImage = newCard.querySelector('.photo__image')
+  const cardDeleteButton = newCard.querySelector('.photo__delete-button')
+  const cardLikeButton = newCard.querySelector('.photo__like-button')
 
-    cardTitle.textContent = card.title;
-    cardImage.setAttribute("src", card.image);
-    cardImage.setAttribute("alt", card.alt);
+  cardTitle.textContent = card.title
+  cardImage.setAttribute('src', card.image)
+  cardImage.setAttribute('alt', card.alt)
 
-    // обработчик события: удаление карточки
-    cardDeleteButton.addEventListener("click", handleDeleteButtonClick);
-    
-    // обработчик события: нравится карточка
-    cardLikeButton.addEventListener("click", handleLikeButtonClick);
-    
-    cardsContainer.append(newCard);
-};
+  // обработчик события: удаление карточки
+  cardDeleteButton.addEventListener('click', handleDeleteButtonClick)
 
-//цикл для массива с определенной функцией (метод forEach принимает функцию в качестве аргумента)
-photoCards.forEach(createCard);
+  // обработчик события: нравится карточка
+  cardLikeButton.addEventListener('click', handleLikeButtonClick)
+
+  // обработчик открытия просмотра изображения cardImage.addEventListener('click', () => console.log('test'));
+  cardImage.addEventListener('click', handleImagePopupOpen => {
+    openPopup(imagePopupOpen)
+    imageTitle.textContent = card.title
+    imagePopup.setAttribute('src', card.image)
+    imagePopup.setAttribute('alt', card.alt)
+  })
+
+  cardsContainer.prepend(newCard)
+}
+
+// выполнение функции создания карточки для каждого элемента (метод forEach принимает функцию в качестве аргумента)
+photoCards.forEach(createCard)
+
+// Обработчик «отправки» данных формы (submit)
+formElementCard.addEventListener('submit', handleCardAdd)
+
+// функция отправки данных формы добавления карточки
+function handleCardAdd (evt) {
+  evt.preventDefault()
+  const title = placeNameInput.value
+  const image = placeLinkInput.value
+  const alt = placeNameInput.value
+  const card = { title, alt, image }
+  createCard(card)
+  closePopup(cardPopup)
+  formElementCard.reset()
+}
 
 // фунуция удаления карточки
- function handleDeleteButtonClick(evt) {
-    const button = evt.target;
-    const photo = button.closest(".photo");
-    photo.remove();
+function handleDeleteButtonClick (evt) {
+  const button = evt.target
+  const photo = button.closest('.photo')
+  photo.remove()
 }
 
 // функция лайка карточки
-function handleLikeButtonClick(evt) {
-    const buttonLike = evt.target;
-    buttonLike.classList.toggle("photo__like-button_active");
+function handleLikeButtonClick (evt) {
+  const buttonLike = evt.target
+  buttonLike.classList.toggle('photo__like-button_active')
 }
 
-//Добавление карточки
+// Обработчики события для добавления карточки по клику: открытие и закрытие
+popupAddCardButton.addEventListener('click', function () {
+  openPopup(cardPopup)
+})
+
+popupCloseButtonCard.addEventListener('click', function () {
+  closePopup(cardPopup)
+  formElementCard.reset()
+})
+
+popupCloseButtonImageOpen.addEventListener('click', function () {
+  closePopup(imagePopupOpen)
+})
